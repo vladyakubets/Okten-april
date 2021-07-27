3a4a1ae33062976a57a8bfa1e44726dcdd23ff50
vladyakubets <jakubetc.vlad@gmail.com>
2021-07-27 19:04:04 +0300

    master cleared

--- classworks/Angular/src/app/app.module.ts
+++ classworks/Angular/src/app/app.module.ts
@@ -3,21 +3,14 @@ import {BrowserModule} from '@angular/platform-browser';
 
 import {AppComponent} from './app.component';
 import {RouterModule, Routes} from "@angular/router";
-import {UsersWithPostsComponent} from './users-with-posts/users-with-posts.component';
-import {UserComponent} from './user/user.component';
 import {HttpClientModule} from "@angular/common/http";
-import {PostComponent} from './post/post.component';
 
 let routes: Routes = [
-  {path: 'users-with-posts', component: UsersWithPostsComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
-    UsersWithPostsComponent,
-    UserComponent,
-    PostComponent
   ],
   imports: [
     BrowserModule,
